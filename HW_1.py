046d0fc4f86999b1c3e957504cdc85f8bb795813
Meiram <133873249+Salam78@users.noreply.github.com>
2023-05-28 17:01:48 +0600

    Delete HW_1.py

--- HW_1.py
+++
@@ -1,8 +0,0 @@
-login = input("Введите логин: ")
-password = input("Введите пароль: ")
-
-if login == "user" and password == "qwerty":
-    print("Authentication completed")
-else:
-    print("Invalid login or password")
-input("press any key to close window")
