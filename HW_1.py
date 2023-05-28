a7bf566f58c620cb4a8579b8a4caf867a5ee22a3
Meiram <133873249+Salam78@users.noreply.github.com>
2023-05-28 16:51:07 +0600

    Add files via upload

---
+++ HW_1.py
@@ -0,0 +1,8 @@
+login = input("Введите логин: ")
+password = input("Введите пароль: ")
+
+if login == "user" and password == "qwerty":
+    print("Authentication completed")
+else:
+    print("Invalid login or password")
+input("press any key to close window")
