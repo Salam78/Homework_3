701dff33a2633a03891ddf2b78767f3d86dfed30
Meiram <133873249+Salam78@users.noreply.github.com>
2023-05-31 18:22:49 +0600

    Add files via upload

---
+++ HW_3.py
@@ -0,0 +1,12 @@
+numbers = []
+
+for i in range(1001):
+    numbers.append(i)
+
+squared_numbers = []
+
+for number in numbers:
+    squared_numbers.append(number ** 2)
+
+print(squared_numbers)
+input("press any key to close window")
